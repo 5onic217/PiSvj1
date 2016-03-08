c071a98af2b2d99ad4f3bd9d847960245645bbcb
student <student@B526-14.fesb.hr>
2016-03-08 10:14:50 +0100

    csvriteline

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,6 +9,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("ffx");
             for (int i = 0; i < 5; i++)
             {
                 for (int j = 0; j < 5-i; j++)
